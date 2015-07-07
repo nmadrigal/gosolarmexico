ca48a34d20dd5ab34b76868759f07dab9b5c1dbe
nmadrigal <nesk27@gmail.com>
2015-07-06 22:05:44 -0500

    addded favicon, updated offices location css and added html markup and css for new referal

---
+++ WebApp/obj/Release/Package/PackageTmp/Scripts/gosolar/Mobile/MobileApp.js
@@ -0,0 +1,16 @@
+﻿GosolarApp = {};
+GosolarApp.Home = {};
+
+(function (g, $) {
+    var self = this;    
+
+    g.Init = function () {
+       
+        g.InitSlider();
+    };
+
+    g.InitSlider = function () {
+        
+    };
+
+})(window.GosolarApp.Home = window.GosolarApp.Home || {}, jQuery);
